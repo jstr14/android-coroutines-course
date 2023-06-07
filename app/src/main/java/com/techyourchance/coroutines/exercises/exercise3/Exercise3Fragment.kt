2ba996ceeae70e1f9c3c1a1328db1f5f9d0fb3dc
Hector Manrique <hector.manrique@free-now.com>
2023-06-07 14:27:04 +0200

    Solve exercise 3

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise3/Exercise3Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise3/Exercise3Fragment.kt
@@ -29,21 +29,25 @@ class Exercise3Fragment : BaseFragment() {
 
     private lateinit var getReputationEndpoint: GetReputationEndpoint
 
-    private var job: Job? = null
+    private var elapsedTimeJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getReputationEndpoint = compositionRoot.getReputationEndpoint
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_exercise_3, container, false)
 
         txtElapsedTime = view.findViewById(R.id.txt_elapsed_time)
 
         edtUserId = view.findViewById(R.id.edt_user_id)
         edtUserId.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 btnGetReputation.isEnabled = !s.isNullOrEmpty()
@@ -55,20 +59,38 @@ class Exercise3Fragment : BaseFragment() {
         btnGetReputation = view.findViewById(R.id.btn_get_reputation)
         btnGetReputation.setOnClickListener {
             logThreadInfo("button callback")
-            job = coroutineScope.launch {
+
+            elapsedTimeJob = coroutineScope.launch {
+                updateElapsedTime()
+            }
+
+            coroutineScope.launch {
                 btnGetReputation.isEnabled = false
                 val reputation = getReputationForUser(edtUserId.text.toString())
-                Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT)
+                    .show()
                 btnGetReputation.isEnabled = true
+                elapsedTimeJob?.cancel()
             }
         }
 
         return view
     }
 
+    private suspend fun updateElapsedTime() {
+        //update elapsed time
+        val currentTime = System.currentTimeMillis()
+        while (true) {
+            delay(1000)
+            val elapsedTime = System.currentTimeMillis() - currentTime
+            val formattedTimeInSeconds = elapsedTime / 1000
+            txtElapsedTime.text = "Elapsed time: $formattedTimeInSeconds seconds"
+        }
+    }
+
     override fun onStop() {
         super.onStop()
-        job?.cancel()
+        coroutineScope.coroutineContext.cancelChildren()
         btnGetReputation.isEnabled = true
     }
 
